10d7c55874692d44a615ab3c9b5f44def690abf6
Pankaj Gupta <gpankajkumar580@gmail.com>
2026-03-01 23:17:49 +0530

    new updated code

--- src/pages/WorkExp/WorkExp.js
+++ src/pages/WorkExp/WorkExp.js
@@ -20,15 +20,34 @@ const WorkExp = () => {
               className="vertical-timeline-element--work"
               contentStyle={{ background: "white", color: "black" }}
               contentArrowStyle={{ borderRight: "7px solid  white" }}
+              date="2021 - 2025"
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              icon={<MdSchool />}
+            >
+              <h3 className="vertical-timeline-element-title">Fresher</h3>
+              <p className="vertical-timeline-element-subtitle">
+                Full Stack Developer (Fresher) skilled in MERN stack (React.js,
+                Node.js, Express.js, MongoDB) with hands-on experience in
+                building responsive web applications and RESTful APIs.
+                Passionate about clean coding, problem-solving, and continuous
+                learning.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "white", color: "black" }}
+              contentArrowStyle={{ borderRight: "7px solid  white" }}
               date="June-2025 - Oct-2025"
               iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
               icon={<MdSchool />}
             >
               <h3 className="vertical-timeline-element-title">
-                Bharat Homeopathy
+                Bharat Homeopathy Pvt. Ltd. in HUDA City, Gurugram.
               </h3>
               <p className="vertical-timeline-element-subtitle">
-                Data Entery Operator pvt ltd
+                Maintained and updated inventory and billing data using MS Excel
+                and internal software Generated daily and monthly reports for
+                management and accounts department. <br />
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -39,30 +58,20 @@ const WorkExp = () => {
               iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
               icon={<MdSchool />}
             >
-              <h5 className="vertical-timeline-element-title">XYZ</h5>
-              <p className="vertical-timeline-element-subtitle">Coding</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentStyle={{ background: "white", color: "black" }}
-              contentArrowStyle={{ borderRight: "7px solid  white" }}
-              date="2021 - 2025"
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-              icon={<MdSchool />}
-            >
-              <h3 className="vertical-timeline-element-title">B Tech</h3>
-              <h4 className="vertical-timeline-element-subtitle">RGPV</h4>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              contentStyle={{ background: "white", color: "black" }}
-              contentArrowStyle={{ borderRight: "7px solid  white" }}
-              date="2021 - 2025"
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-              icon={<MdSchool />}
-            >
-              <h3 className="vertical-timeline-element-title">B Tech</h3>
-              <h4 className="vertical-timeline-element-subtitle">RGPV</h4>
+              <h5 className="vertical-timeline-element-title">
+                Som Company ( Reatil Company)
+              </h5>
+              <p className="vertical-timeline-element-subtitle">
+                {" "}
+                I worked as a Data Entry Operator at Bharat Homeopathy Pvt.
+                Ltd.I handled large volumes of data using MS Excel and internal
+                software, verified entries to avoid errors, and generated daily
+                reports for management I handled large volumes of data using MS
+                Excel and internal software, verified entries to avoid errors,
+                and generated daily reports for management This role helped me
+                develop strong attention to detail, data management skills, and
+                efficiency in handling administrative tasks.
+              </p>
             </VerticalTimelineElement>
           </VerticalTimeline>
         </div>
